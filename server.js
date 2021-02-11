5d678618139bc716890614739ae3a9ca89f11539
Dimitar Zahariev <dbzahariev@gmail.com>
2021-02-11 23:37:50 +0200

    make it better

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongoose.connect(process.env.DATABASE_URL, {
 });
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
-db.once("open", () => console.log("Connected to Mongoose"));
+db.once("open", () => console.log("Connected to Mongoose and heroku"));
 
 app.use("/", indexRouter);
 
